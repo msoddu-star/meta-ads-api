45a090520f9b4a63dc1cb99670291980e8e4cc65
msoddu-star <m.soddu@merlinwizard.com>
2026-06-23 17:58:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,16 +5,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors({ origin: '*' }));
 app.options('*', cors({ origin: '*' }));
+app.use(express.json());
 
 app.get('/campaigns', async (req, res) => {
   try {
-    const token = req.query.token || process.env.META_ACCESS_TOKEN;
-    const accountId = req.query.account_id || process.env.META_AD_ACCOUNT_ID;
-
-    if (!token || !accountId) {
-      return res.status(400).json({ error: 'token e account_id sono obbligatori' });
-    }
-
+    const token = process.env.META_ACCESS_TOKEN;
+    const accountId = process.env.META_AD_ACCOUNT_ID;
     const response = await fetch(
       `https://graph.facebook.com/v25.0/${accountId}/campaigns?fields=id,name,status,objective,daily_budget,lifetime_budget,start_time,stop_time&access_token=${token}`
     );
@@ -26,12 +22,7 @@ app.get('/campaigns', async (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.json({
-    message: 'Meta Ads API nuova versione',
-    routes: ['/campaigns']
-  });
+  res.json({ message: 'Meta Ads API', routes: ['/campaigns'] });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
